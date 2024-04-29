626f2ce4248dcb80a4360fd76d73755a663fe711
seojeongHHHui <sjhui0523@gmail.com>
2024-04-29 13:50:43 +0900

    upload practice04

---
+++ chapter04/src/main/java/chapter04/CalendarTest.java
@@ -0,0 +1,19 @@
+package chapter04;
+
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class CalendarTest {
+
+	public static void main(String[] args) {
+		Calendar.getInstance();
+		
+//		Locale aLocale = Locale.getDefault(Locale.Category.FORMAT);
+//		TimeZone tz = TimeZone.getDefault();
+//		System.out.println(aLocale + ":" + tz);
+		
+		System.out.println();
+	}
+
+}
---
+++ practice04/src/main/java/prob04/Prob004.java
@@ -0,0 +1,9 @@
+package prob04;
+
+public class Prob004 {
+
+	public static void main(String[] args) {
+		Employee pt = new Depart( "홍길동", 3000, "개발부" );
+		pt.getInformation();
+	}
+}
