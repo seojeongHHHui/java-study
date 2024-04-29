9bab5ea9a17eb1a4e51b7f3a267d9c71bc4cf838
seojeongHHHui <sjhui0523@gmail.com>
2024-04-29 14:31:02 +0900

    Calendar #finish

--- chapter04/src/main/java/chapter04/CalendarTest.java
+++ chapter04/src/main/java/chapter04/CalendarTest.java
@@ -7,13 +7,41 @@ import java.util.TimeZone;
 public class CalendarTest {
 
 	public static void main(String[] args) {
-		Calendar.getInstance();
 		
 //		Locale aLocale = Locale.getDefault(Locale.Category.FORMAT);
 //		TimeZone tz = TimeZone.getDefault();
 //		System.out.println(aLocale + ":" + tz);
 		
-		System.out.println();
+		Calendar cal = Calendar.getInstance();
+		printDate(cal);
+		
+		cal.set(Calendar.YEAR, 2024);
+		cal.set(Calendar.MONTH, 11); // 12월
+		cal.set(Calendar.DATE, 25);
+		printDate(cal);
+		
+		cal.set(2001, 7, 13);
+		cal.add(Calendar.DATE, 10000);
+	}
+	
+	private static void printDate(Calendar cal) {
+		final String[] DAYS = {"일", "월", "화", "수", "목", "금", "토"};
+		
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH); // 0~11, +1
+		int date = cal.get(Calendar.DATE);
+		int day = cal.get(Calendar.DAY_OF_WEEK); // 1(일)~7(토)
+		int hour = cal.get(Calendar.HOUR);
+		int minute = cal.get(Calendar.MINUTE);
+		int second = cal.get(Calendar.SECOND);
+		
+		System.out.println(
+				year + "년 " +
+				(month+1) + "월 " + 
+				date + "일 " + 
+				hour + "시 " +
+				minute + "분 " + 
+				second + "초");
 	}
 
 }
