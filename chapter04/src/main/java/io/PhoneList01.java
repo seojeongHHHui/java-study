d0e69584fd8eb0223ffd1e876f0f99b226113ae9
seojeongHHHui <sjhui0523@gmail.com>
2024-04-30 14:26:28 +0900

    io #5-1

--- chapter04/src/main/java/io/PhoneList01.java
+++ chapter04/src/main/java/io/PhoneList01.java
@@ -1,15 +1,21 @@
 package io;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.StringTokenizer;
 
 public class PhoneList01 {
 
 	public static void main(String[] args) {
+		BufferedReader br = null;
+		
 		try {
 			File file = new File("./phone.txt");
 			if(!file.exists()) {
@@ -27,11 +33,53 @@ public class PhoneList01 {
 			
 			System.out.println("=== 전화번호 ===");
 			
+			// 1. 기반 스트림			
 			FileInputStream fis = new FileInputStream(file);
-			fis.read();
-		}  catch (IOException e) {
 			
-		} 
+			// 2. 보조 스트림 02 (byte|byte|byte => char)
+			InputStreamReader isr = new InputStreamReader(fis, "utf-8");
+			
+			// 3. 보조 스트림 03 (char|char|char|\n -> "charcharchar")
+			br = new BufferedReader(isr);
+			
+			// 4. 처리
+			String line = null;
+			while((line = br.readLine()) != null) {
+				//System.out.println(line);
+				StringTokenizer st = new StringTokenizer(line, "\t ");
+				
+				int index = 0;
+				while(st.hasMoreElements()) {
+					String token = st.nextToken();
+					
+					if(index == 0) { // 이름
+						System.out.print(token + ":");
+					} else if(index == 1) { // 전화번호1
+						System.out.print(token + "-");
+					} else if(index == 2) { // 전화번호2
+						System.out.print(token + "-");
+					} else { // 전화번호3				
+						System.out.print(token + "\n");
+					}
+					
+					index++;
+				}
+			}
+			
+		} catch (UnsupportedEncodingException e) {
+			System.out.println("error: " + e);
+		} catch (IOException e) {
+			System.out.println("error: " + e);
+		} finally {
+			try {
+				if(br != null) {
+					br.close();
+				}
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 }
