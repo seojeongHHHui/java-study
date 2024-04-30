47fcb52a309b1bc63a7c24c03a1787b6da8910f6
seojeongHHHui <sjhui0523@gmail.com>
2024-04-30 14:33:46 +0900

    chore: io #5-1

--- chapter04/src/main/java/io/PhoneList01.java
+++ chapter04/src/main/java/io/PhoneList01.java
@@ -26,10 +26,10 @@ public class PhoneList01 {
 			System.out.println("=== 파일정보 ===");
 			System.out.println(file.getAbsolutePath());
 			System.out.println(file.length() + "Bytes");
-			System.out.println(file.lastModified());
+			//System.out.println(file.lastModified());
 			
-			Date d = new Date(file.lastModified());
-			System.out.println(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(d));
+			//Date d = new Date(file.lastModified());
+			System.out.println(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date(file.lastModified())));
 			
 			System.out.println("=== 전화번호 ===");
 			
